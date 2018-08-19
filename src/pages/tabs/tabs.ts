88aa13ca46f63737e905efba9dba8211e13dc824
Javier Lozano <javier_l_o@hotmail.com>
2018-08-19 17:19:04 -0500

    se crean components para pendientes y terminados, se quitan pages sin uso

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { pendientesPage } from '../pendientes/pendientes.page';
+import { terminadosPage } from '../terminados/terminados.page';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = pendientesPage;
+  tab2Root = terminadosPage;
 
   constructor() {
 
