de5da043240d4a5f74b1814d03c9f1bd1b56f113
Javier Lozano <javier_l_o@hotmail.com>
2018-08-20 03:16:51 -0500

    Se agregan los items a la lista

--- src/pages/agregar/agregar.page.ts
+++ src/pages/agregar/agregar.page.ts
@@ -1,26 +1,42 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 import { Lista } from '../../models';
 import { DeseosService } from '../../services/deseos.service';
-import { NavParams } from "ionic-angular";
+import { NavParams } from 'ionic-angular';
+import { ListaItem } from '../../models/lista-item.model';
 
 @Component({
-    selector: 'page-agregar',
-    templateUrl : 'agregar.page.html'
+  selector: 'page-agregar',
+  templateUrl: 'agregar.page.html'
 })
 export class AgregarPage {
+  lista: Lista;
+  nombreItem: string = '';
 
-lista: Lista;
+  constructor(
+    public deseosService: DeseosService,
+    private navParams: NavParams
+  ) {
+    console.log(this.navParams.get('tituloLista'));
+    const titulo = this.navParams.get('tituloLista');
+    this.lista = new Lista(titulo);
+  }
 
-    constructor (public deseosService: DeseosService,
-                 private navParams: NavParams) {
-                     console.log(this.navParams.get('tituloLista'));
-                     const titulo = this.navParams.get('tituloLista');
-                     this.lista = new Lista(titulo);
-                 }
-
-
-    listaSeleccionada( lista: Lista) {
-
+  agregarItem() {
+    if (this.nombreItem.length === 0) {
+      return;
     }
 
+    if (this.nombreItem.trim() === '') {
+      return;
+    }
+
+    const nuevoItem = new ListaItem(this.nombreItem);
+    this.lista.items.push(nuevoItem);
+
+    this.nombreItem = '';
+  }
+
+  actualizarTarea(item: ListaItem) {
+    item.completado = !item.completado;
+  }
 }
