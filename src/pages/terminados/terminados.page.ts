5a127706f52c627263a05430e65f3fce520ad19f
Javier Lozano <javier_l_o@hotmail.com>
2018-08-20 01:14:57 -0500

    se cambia color primario,se crean listas y se importan imgs

--- src/pages/terminados/terminados.page.ts
+++ src/pages/terminados/terminados.page.ts
@@ -1,9 +1,17 @@
 import { Component } from "@angular/core";
+import { Lista } from '../../models';
+import { DeseosService } from '../../services/deseos.service';
 
 @Component({
     selector: 'page-terminados',
     templateUrl : 'terminados.page.html'
 })
 export class TerminadosPage {
+    constructor (public deseosService: DeseosService) {}
+
+
+    listaSeleccionada( lista: Lista) {
+
+    }
 
 }
