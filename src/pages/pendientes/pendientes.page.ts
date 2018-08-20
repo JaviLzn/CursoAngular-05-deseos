2c60ec8a46bd3b21038c6e112237c72dde6e1606
Javier Lozano <javier_l_o@hotmail.com>
2018-08-20 00:58:59 -0500

    Se modelo y servicio para lista

--- src/pages/pendientes/pendientes.page.ts
+++ src/pages/pendientes/pendientes.page.ts
@@ -1,9 +1,16 @@
 import { Component } from "@angular/core";
+import { DeseosService } from '../../services/deseos.service';
+import { Lista } from '../../models';
 
 @Component({
     selector : 'page-pendientes',
     templateUrl: 'pendientes.page.html'
 })
-export class pendientesPage {
+export class PendientesPage {
 
+    constructor (public deseosService: DeseosService) {
+                
+    }
+
+    listaSeleccionada ( lista: Lista) {}
 }
